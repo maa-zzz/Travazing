3886ab46b6e12e37cd4f84df65870d66f9e3056d
Md <maaz.yusra.md@gmail.com>
2023-06-27 21:08:43 +0530

    without my bookings

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CommonSection from "../shared/CommonSection";
+import Newsletter from "./../shared/Newsletter";
+
+const About = () => {
+  return (
+    <>
+      <CommonSection title={"About"} />
+
+      <Newsletter />
+    </>
+  );
+};
+
+export default About;
